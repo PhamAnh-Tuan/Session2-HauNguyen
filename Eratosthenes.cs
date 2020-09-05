9bf7e0c9781b8dae5fb46afdfb44a2ed52188cd8
PhamAnh-Tuan <tuanphamacb05@gmail.com>
2020-09-05 22:08:17 +0700

    Add new file

---
+++ Eratosthenes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eratosthenes
+{
+    class Program
+    {
+        static int n;
+        static void Nhap()
+        {
+            do
+            {
+                Console.Write("Nhap n:");
+                n = int.Parse(Console.ReadLine());
+            } while (n <= 2);
+        }
+        static void SNT()
+        {
+            bool[] isPrime=new bool[n+1];
+            for (int i = 0; i <= n; ++i)
+            {
+                isPrime[i] = true;
+            }
+            isPrime[0] = false;
+            isPrime[1] = false;
+            for (int i = 2; i * i <= n; ++i)
+            {
+                if (isPrime[i] == true)
+                {
+                    for (int j = i * i; j <= n; j += i)
+                        isPrime[j] = false;
+                }
+            }
+            for(int i = 2; i <= n; i++)
+            {
+                if (isPrime[i])
+                    Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Nhap();
+            SNT();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Euclid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euclid
+{
+    class Program
+    {
+        static int Tinh(int a, int b)
+        {
+            if (b == 0) return a;
+            return Tinh(b, a % b);
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Nhap a:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap b:");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Tinh(a, b));
+            Console.ReadKey();
+        }
+    }
+}
